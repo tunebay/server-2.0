2a6891e42c84d87fbba727116fd622994c47dc4f
Mali Michael <malimccalla@gmail.com>
2017-05-21 22:03:08 +0100

    Stub lib auth tests and change name of user models tests file for consistency

---
+++ test/lib/auth.test.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import {
+  generateToken,
+  getUserIdFromToken,
+  setUserInfo,
+  comparePassword
+} from '../../lib/auth';
+
+xdescribe('🔑 Auth', () => {
+  beforeEach(() => {
+  });
+
+  describe('generateToken()', () => {
+    it('generates a token for a given user', () => {
+
+    });
+  });
+});
