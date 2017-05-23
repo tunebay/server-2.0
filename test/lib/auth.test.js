77d7a610862734157e6528221953f8f23865d6be
Mali Michael <malimccalla@gmail.com>
2017-05-23 15:51:56 +0100

    Adds displayName to set user info and tests

--- test/lib/auth.test.js
+++ test/lib/auth.test.js
@@ -1,18 +1,30 @@
 import { expect } from 'chai';
-import {
-  generateToken,
-  getUserIdFromToken,
-  setUserInfo,
-  comparePassword
-} from '../../lib/auth';
+import { generateToken, setUserInfo } from '../../lib/auth';
 
-xdescribe('🔑 Auth', () => {
-  beforeEach(() => {
+describe('🔑 Auth', () => {
+  const newUser = {
+    id: 1,
+    display_name: 'John Doe',
+    email: 'john@example.com',
+    username: 'johndoe'
+  };
+
+  describe('setUserInfo()', () => {
+    it('sets the user info', () => {
+      expect(setUserInfo(newUser)).to.have.property('email');
+      expect(setUserInfo(newUser)).to.have.property('id');
+      expect(setUserInfo(newUser)).to.have.property('displayName');
+      expect(setUserInfo(newUser)).to.have.property('username');
+      expect(setUserInfo(newUser)).not.to.have.property('passwordHash');
+      expect(setUserInfo(newUser)).not.to.have.property('password_hash');
+      expect(setUserInfo(newUser)).not.to.have.property('password');
+    });
   });
 
   describe('generateToken()', () => {
-    it('generates a token for a given user', () => {
-
+    it('generates a JWT token for the given user', () => {
+      const userInfo = setUserInfo(newUser);
+      expect(generateToken(userInfo)).to.be.a('string');
     });
   });
 });
