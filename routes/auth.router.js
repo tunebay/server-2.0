a4f0c53992bb4d77259501b51f130b13472b6e3e
Mali Michael <malimccalla@gmail.com>
2017-05-23 17:22:39 +0100

    Adds login route to router

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { register } from '../controllers/auth.controller';
+import passport from 'passport';
+import { register, login } from '../controllers/auth.controller';
+import '../config/passport';
 
 const router = express.Router();
 
+// const requireAuth = passport.authenticate('jwt', { session: false });
+const requireLogin = passport.authenticate('local', { session: false });
+
 router.post('/register', register);
+router.post('/login', requireLogin, login);
 
 export default router;
