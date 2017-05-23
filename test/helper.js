372e86cb04d9d140232083fdb911f098c6134879
Mali Michael <malimccalla@gmail.com>
2017-05-23 11:12:36 +0100

    Add db cleaner functionality to test helper

--- test/helper.js
+++ test/helper.js
@@ -1,3 +1,29 @@
 import chai from 'chai';
+import knex from '../db/knex';
 
 chai.use(require('chai-json-schema'));
+
+const tables = [
+  'users'
+];
+
+const truncate = () => {
+  return Promise.each(tables, (table) => {
+    return knex.raw(`TRUNCATE TABLE ${table} RESTART IDENTITY CASCADE`);
+  });
+};
+
+if (process.env.NODE_ENV === 'test') {
+  beforeEach((done) => {
+    truncate()
+    .then(() => {
+      knex.migrate.latest()
+      .then(() => knex.seed.run() // seeds 12 users
+      .then(() => done()));
+    });
+  });
+
+  afterEach((done) => {
+    truncate().then(() => done());
+  });
+}
