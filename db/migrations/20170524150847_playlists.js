6d5eb021b7925b37c21c3687a64d19c7fd7768cb
Mali Michael <malimccalla@gmail.com>
2017-05-24 16:04:55 +0100

    Adds migration for create playlist table

---
+++ db/migrations/20170524150847_playlists.js
@@ -0,0 +1,22 @@
+exports.up = (knex) => {
+  return knex.schema.createTable('playlists', (t) => {
+    t.increments('id').notNullable().primary();
+    t.integer('user_id').unsigned();
+    t.foreign('user_id').references('id').inTable('users');
+    t.string('title', 100).notNullable();
+    t.string('playlist_type', 24).notNullable();
+    t.float('price', 5, 2).notNullable();
+    t.integer('number_of_tracks').notNullable();
+    t.integer('duration').notNullable();
+    t.boolean('can_pay_more').notNullable();
+    t.string('permalink').notNullable().unique();
+    t.string('artwork').nullable();
+    t.string('purchase_message', 255).nullable();
+    t.timestamp('release_date').nullable();
+    t.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('playlists');
+};
