fab5f965558df39afb52b5636ea0187e06da3507
Mali Michael <malimccalla@gmail.com>
2017-05-25 14:58:51 +0100

    Set up playlist routes

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,7 +1,9 @@
 import express from 'express';
+
 import userRoutes from './user.router';
 import authRoutes from './auth.router';
 import awsRoutes from './aws.router';
+import playlistRoutes from './playlist.router';
 
 const router = express.Router();
 
@@ -15,5 +17,6 @@ router.get('/', (req, res) => {
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/aws', awsRoutes);
+router.use('/playlists', playlistRoutes);
 
 export default router;
