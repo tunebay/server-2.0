331580aaca12f0dda3ebbe0b3432333996e81c92
Mali Michael <malimccalla@gmail.com>
2017-05-25 11:40:54 +0100

    Create basic playlists seed file and prefix seed files to run in order

---
+++ db/seeds/development/02_playlists_seed.js
@@ -0,0 +1,37 @@
+const faker = require('faker');
+
+exports.seed = (knex) => {
+  return knex('playlists').del() // Deletes ALL existing entries and restart sequence
+    .then(() => { // Inserts seed entries one by one in series
+      // 1
+      return knex('playlists').insert({
+        user_id: 1,
+        title: 'Alchemy',
+        playlist_type: 'album',
+        price: 8.99,
+        number_of_tracks: 11,
+        duration: 2580,
+        can_pay_more: true,
+        permalink: 'https://tunebay.com/malimichael/alchemy',
+        artwork: 'https://tunebay-upload.s3-eu-west-2.amazonaws.com/users/artwork/85cd2abe-3a96-4d9c-91a2-b4cb066709c4',
+        purchase_message: 'Thanks for the support',
+        release_date: faker.date.past(),
+        created_at: faker.date.past(),
+      });
+    })
+    .then(() => {
+      // 2
+      return knex('playlists').insert({
+        user_id: 1,
+        title: 'Afterlife EP',
+        playlist_type: 'EP',
+        price: 0.00,
+        number_of_tracks: 4,
+        duration: 982,
+        can_pay_more: true,
+        permalink: 'https://tunebay.com/malimichael/afterlife-ep',
+        release_date: faker.date.past(),
+        created_at: faker.date.past(),
+      });
+    });
+};
