0014926dfd990b57b2c09ceff3cc3009944e990c
Mali Michael <malimccalla@gmail.com>
2017-06-03 22:08:26 +0100

    Write tracks migration

--- db/migrations/20170602082333_tracks.js
+++ db/migrations/20170602082333_tracks.js
@@ -1,8 +1,18 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = (knex) => {
+  return knex.schema.createTable('tracks', (t) => {
+    t.increments('id').notNullable().primary();
+    t.integer('playlist_id').unsigned();
+    t.foreign('playlist_id').references('id').inTable('playlists').onDelete('CASCADE');
+    t.string('name', 100).notNullable();
+    t.integer('duration').notNullable();
+    t.integer('playlist_position').notNullable();
+    t.float('price', 5, 2);
+    t.boolean('single').notNullable();
+    t.string('file_type').notNullable();
+    t.string('location', 255).nullable();
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('tracks');
 };
