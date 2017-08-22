01a837e59e2dc3e28eb7d147027e37e4a21c394d
Mali Michael <malimccalla@gmail.com>
2017-08-22 14:20:27 +0100

    Adds socials table migration

---
+++ db/migrations/20170822141428_socials.js
@@ -0,0 +1,12 @@
+exports.up = (knex) => {
+  return knex.schema.createTable('socials', (t) => {
+    t.integer('user_id').unsigned();
+    t.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
+    t.string('social_id').notNullable();
+    t.string('provider').notNullable();
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('socials');
+};
